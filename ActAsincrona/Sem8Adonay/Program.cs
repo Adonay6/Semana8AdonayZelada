bbb8bd3276eb86f215630191b5d06d597019d96d
Adonay66 <Zeladaadonay7@gmail.com>
2023-03-17 22:09:06 -0600

    Conexion con bd
    Visual con SQL

---
+++ ActAsincrona/Sem8Adonay/Program.cs
@@ -0,0 +1,30 @@
+﻿using Sem8Adonay.Models;
+
+using AlmacenContext db = new AlmacenContext();
+
+Producto product= new Producto();
+
+Console.Write("Ingrese el nombre del producto -> ");
+product.Nombre = Console.ReadLine();
+
+Console.Write("Ingrese una descripcion del producto -> ");
+product.Descripcion = Console.ReadLine();
+
+Console.Write("Ingrese el precio del producto -> ");
+product.Precio = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Ingrese la cantidad de productos existentes -> ");
+product.Stock = Convert.ToInt32(Console.ReadLine());
+
+db.Productos.Add(product);
+db.SaveChanges();
+
+var ListProductos = db.Productos.ToList();
+
+foreach (var i in ListProductos)
+{
+    Console.WriteLine($"\nNombre: {i.Nombre}");
+    Console.WriteLine($"Descripcion: {i.Descripcion}");
+    Console.WriteLine($"Precio: ${i.Precio}");
+    Console.WriteLine($"Stock {i.Stock}");
+}
